1fc4dd3f5745a0eb1d8894acbeca5e7d3ecbc9e8
iceblue11 <Cyb3r-Assassin@cyberpunk.me>
2014-03-02 01:06:02 -0600

    Version 0.8 Pentoo RC2 Special

--- monitor.sh
+++ monitor.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-#retrieve absolute path structures so we can use symlinks and config files
-SOURCE="${BASH_SOURCE[0]}"
-while [[ -h "$SOURCE" ]]; do # resolve $SOURCE until the file is no longer a symlink
-    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-    SOURCE="$(readlink "$SOURCE")"
-    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it's relativeness to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 BLD_RED=${txtbld}$(tput setaf 1) # red
 TXT_RST=$(tput sgr0)             # Reset
 echo "" > /var/log/messages
@@ -16,7 +8,7 @@ while true; do
 	do gotchya=$(egrep 'DHCPACK|$1' | awk -Fon '{print $2'})
 	done < /var/log/messages
 	clear
-	echo "${BLD_TEA}$(cat $DIR/monitor.logo)${TXT_RST}"
+	echo "${BLD_TEA}$(cat /usr/share/n4p/monitor.logo)${TXT_RST}"
 	echo -e "$gotchya \n"
 	sleep 8
 done
